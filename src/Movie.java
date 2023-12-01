256a4584207178ffbabc0fb04e144370eb18f223
MalakDiab-04 <malak.amir.diab.0901@gmail.com>
2023-12-01 10:59:46 +0200

    committ main

--- src/Movie.java
+++ src/Movie.java
@@ -35,4 +35,52 @@ public class Movie {
         this.languages = languages;
         this.poster = poster;
     }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public LocalTime getDurationTime() {
+        return durationTime;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public String[] getGenre() {
+        return genre;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public float getBudget() {
+        return budget;
+    }
+
+    public float getRevenue() {
+        return revenue;
+    }
+
+    public float getImbd_score() {
+        return imbd_score;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
 }
